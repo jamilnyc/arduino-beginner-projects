d013cc3a16bf50d6337a19daa25953c33b90f4f4
Jamil <murshed.ahmed92@gmail.com>
2016-04-17 18:18:42 -0400

    Added comments and connection instructions to rgb.ino

--- rgb/rgb.ino
+++ rgb/rgb.ino
@@ -1,6 +1,22 @@
 /**
  * Reads an analog value and uses it to dim a color of an RGB
  * LED. The color is controlled by a button.
+ * 
+ * Connections:
+ * 1) Pin 1 of 50k potentiometer (POT) to ground (GND)
+ * 2) Pin 3 of POT to 5 volts (VCC)
+ * 3) Pin 2 of POT to analog input A0
+ * 4) Pin 1 of push button to VCC
+ * 5) Pin 2 of push button to digital pin 2
+ * 6) Digital pin 2 to pin 1 of resistor R4 (10M)
+ * 7) R4 pin 2 to GND
+ * 8) Pin 11 to pin 1 of R1 (220 ohms)
+ * 9) Pin 2 of R1 to RGB LED red pin
+ * 10) Pin 10 to pin 1 of R2 (220 ohms)
+ * 11) Pin 2 of R2  to RGB LED green pin
+ * 12) Pin 11 to pin 1 of R3 (220 ohms)
+ * 13) Pin 2 of R3 to RGB LED blue pin
+ * 14) Cathode of RGB LED to GND
  */
 
 #define BUTTON_RED 0
@@ -32,6 +48,7 @@ void loop() {
   int buttonValue = digitalRead(2);
   
 
+  // Cycle through color modes
   if (buttonValue == HIGH) {
     if (buttonMode == BUTTON_RED) {
       buttonMode = BUTTON_GREEN;
@@ -41,6 +58,8 @@ void loop() {
       buttonMode = BUTTON_RED;
     }
   }
+
+  // Read value from potentiometer and convert it to range 0-255
   sensorValue = analogRead(sensorPin);
   sensorValue = map(sensorValue, 0, 1023, 0, 255);
   
@@ -66,19 +85,5 @@ void setColor(int red, int green, int blue)
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);
-}
-
-// Not used
-// Cycles through all colors
-void cycle() {
-  int interval = 10;
-  for (int i = 0; i <= 255; i += interval) {
-    for (int j = 0; j <= 255; j += interval) {
-      for (int k = 0; k <= 255; k += interval) {
-        setColor(i, j, k);
-        delay(10);
-      }
-    }
-  }
 }
 
